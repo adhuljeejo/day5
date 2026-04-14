b62f767fd11fa2f7af95679695d91fd60718875f
adhuljeejo <adhuljeejo873@gmail.com>
2026-04-15 00:09:13 +0530

    Update api-service.ts

--- src/app/api-service.ts
+++ src/app/api-service.ts
@@ -8,12 +8,12 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
-  // ✅ Get all jewellery products
+  
   getproduct() {
     return this.http.get("https://fakestoreapi.com/products/category/jewelery");
   }
 
-  // ✅ Get single product details by ID
+
   getproductdetailsId(id: number) {
     return this.http.get(`https://fakestoreapi.com/products/${id}`);
   }
